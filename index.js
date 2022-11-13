b0336e18dc16ead4c6f58a43a50afc6457d42561
Molly <mojonesycreative@gmail.com>
2022-11-12 19:21:59 -0500

    navToggle - fix later

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ const nav = document.querySelector(".nav");
 navToggle.addEventListener("click", () => {
     if (nav.style.display === "block") {
         nav.style.display = "none";
+        document.body.classList.toggle(".nav-open");
     } else {
         nav.style.display = "block";
+        document.body.classList.toggle(".nav-open");
+        navToggle.style.zIndex = "110";
     }
 });
+
+
+// window.addEventListener("click", (e) => {
+//     if (nav.style.display === "block" && !nav.contains(e.target)) {
+//         nav.style.display = "none";
+//         document.body.classList.toggle(".nav-open");
+//     }
+// });
